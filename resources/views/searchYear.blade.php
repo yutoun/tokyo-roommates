4cc68c404fded8859298b1d10f0902269cb0c67f
yutoun <yutounwasese@gmail.com>
2020-05-21 09:43:52 +0900

    before adding ADD method on sql

--- resources/views/searchYear.blade.php
+++ resources/views/searchYear.blade.php
@@ -1,36 +1,3 @@
-<div class="years">
-  {{ Form::open(['method'=>'get']) }}
-  {{ csrf_field()}}
-  <?php
-  $arr=[];
-  for ($i=0; $i<100; $i++) {
-    $arr[]=$i;
-  }
-  ?>
-  <div class="form-group">
-    {{ Form::label('age','age:') }}<!-- 第一引数はshopのデータ持ってきてるんじゃなくてここで定義した物をstoreに入れる用 -->
-    {{ Form::select('age',[$arr],['class'=>'form-control']) }}
-  </div>
-  <div class="form-group">
-    {{ Form::submit('検索',['class'=>'btn btn-outline-primary']) }}
-  </div>
-  {{ Form::close() }}
-</div>
-
-<div class="room">
-  {{ Form::open(['method'=>'get']) }}
-  {{ csrf_field()}}
-
-  <div class="form-group">
-    {{ Form::label('room','room:') }}<!-- 第一引数はshopのデータ持ってきてるんじゃなくてここで定義した物をstoreに入れる用 -->
-    {{ Form::select('room',['with room' => 'with room',
- 'without room' => 'without room'],['class'=>'form-control']) }}<!-- 一個目がvalue,2個目が表示 -->
-  </div>
-  <div class="form-group">
-    {{ Form::submit('検索',['class'=>'btn btn-outline-primary']) }}
-  </div>
-  {{ Form::close() }}
-</div>
 
 
   <style >
